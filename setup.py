a76862cb72f74ea32d8c2229dbd916537e950863
Patrick Korus <korus@gmx.net>
2020-09-20 15:54:27 +0200

    - fixed setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='mcts-general',
     version='1.0.1',
-    packages=['common', 'mcts_general'],
+    packages=['mcts_general'],
     url='https://github.com/PatrickKorus/mcts-general.git',
     license='GPL-3.0 License',
     author='Patrick Korus',
