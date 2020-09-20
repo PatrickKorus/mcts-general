c7e50e68ec21b638f90c756606207aeec96d5209
Patrick Korus <korus@gmx.net>
2020-09-20 12:58:01 +0200

    - added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mcts-general',
+    version='1.0.0',
+    packages=['common', 'mcts_general'],
+    url='https://github.com/PatrickKorus/mcts-general.git',
+    license='GPL-3.0 License',
+    author='Patrick Korus',
+    author_email='patrick.korus@stud.tu-darmstadt.de',
+    description='A general implementation of Monte Carlo Tree Search for the use with Gym environments.'
+)
